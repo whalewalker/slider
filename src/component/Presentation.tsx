a84f440072d0bef3bcf989af6542e601af48524a
whalewalker <mongodbms@gmail.com>
2023-11-29 15:23:49 +0100

    Implement full screen

--- src/component/Presentation.tsx
+++ src/component/Presentation.tsx
@@ -18,7 +18,7 @@ const Presentation: React.FC<IPresentation> = ({mediaList, ...otherProps}) => {
     return (
         <FullScreen handle={handle}>
             <div className="mx-auto relative overflow-hidden my-10"
-                 style={{width: `${handle.active ? '100%' : '50rem'}`}}>
+                style={{ width: `${handle.active ? '100%' : '50rem' }`, height: `${isAccessibility && '100%'}`}}>
                 <PresentationHeader
                     title={otherProps.title}
                     mediaLength={mediaLength}
@@ -28,13 +28,10 @@ const Presentation: React.FC<IPresentation> = ({mediaList, ...otherProps}) => {
                     setDisplayPageTitle={setDisplayPageTitle}
                     setIsAccessibility={setIsAccessibility}
                 />
-                {isAccessibility ?
-                    <object data={mediaList && mediaList[0].path.replace(/&export=download/, '')} type="application/pdf"
-                            width="100%" height="100%"/>
-                    
-                    :
 
-                    <SliderContainer
+                {isAccessibility ?
+                    <object aria-label="PDF file" data={mediaList && mediaList[0].path.replace(/&export=download/, '')} type="application/pdf" width="100%" height="100%"/>
+                    : <SliderContainer
                         displayPageTitle={displayPageTitle}
                         navigationIndex={navigationIndex}
                         setNavigationIndex={setNavigationIndex}
